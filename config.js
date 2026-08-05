ae421271c8171f5de07e620bf23dea8aea81eb6b
sachsenanhalt22-cyber <sachsen.anhalt22@gmail.com>
2026-08-05 23:58:32 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    prefix: "!",
+    owner: "015566376889",
+
+    botName: "Community Bot",
+
+    startingMoney: 0,
+    startingXP: 0,
+
+    maxWarningsDefault: 5,
+
+    gameMoneyMin: 0,
+    gameMoneyMax: 300,
+
+    gameXPMin: 0,
+    gameXPMax: 300,
+
+    bigEventMoney: 3000,
+    bigEventXP: 2000,
+
+    bigEventDuration: 3 * 24 * 60 * 60 * 1000
+};
